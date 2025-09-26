a04e16ac81b752f01f95e7994a87054e5fc4a63e
ZenturioTech <contact@zenturiotech.com>
2025-09-26 05:03:43 +0000

    dream vibes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero: React.FC = () => {
 
                     {/* Text Content with Glass Effect */}
                     {/* On mobile, it's centered with max-width. On desktop, it aligns right and is wider. */}
-                    <div className="relative z-10 bg-black/20 backdrop-blur-md rounded-3xl p-6 sm:p-10  shadow-xl max-w-lg lg:max-w-none lg:w-[75%] text-center lg:-translate-x-20 lg:pr-32 lg:py-20">
+                    <div className="relative z-10 md:ml-60 bg-black/20 backdrop-blur-md rounded-3xl p-6 sm:p-10  shadow-xl max-w-lg lg:max-w-none lg:w-[75%] text-center lg:-translate-x-20 lg:pr-32 lg:py-20">
                         <p className="text-3xl sm:text-5xl font-serif " style={{ textShadow: '1px 1px 3px rgba(0,0,0,0.4)' }}>Welcome to</p>
                         <h1 className="text-5xl sm:text-7xl font-bold font-serif" style={{ textShadow: '2px 2px 5px rgba(0,0,0,0.5)' }}>Dream Vibes</h1>
                     </div>
@@ -34,8 +34,8 @@ const Hero: React.FC = () => {
 
                 {/* Character Image */}
                 {/* Absolutely positioned relative to the max-w-7xl container */}
-                <div className="absolute z-20 bottom-0 right-0 w-40 sm:w-80 md:w-[28rem] md:h-[28rem]lg:w-[35rem] max-w-[90vw] lg:max-w-none lg:right-4 xl:right-8">
-                    <img src="/images/gb.png" alt="Cartoon boy and girl with backpacks" className="w-full h-auto" />
+                <div className="absolute z-20 bottom-0 right-0  w-40 sm:w-80 md:w-[28rem] md:h-[28rem]lg:w-[35rem] max-w-[90vw] lg:max-w-none lg:right-10 xl:right-1">
+                    <img src="/images/gb.png" alt="Cartoon boy and girl with backpacks" className="w-full h-auto " />
                 </div>
             </div>
         </section>
